ca6eb59fac8d8a1ba27157bb9955eb8691105f50
Dpalme <diegoPalmerin24@gmail.com>
2020-02-04 07:07:11 -0600

    fuck if i know

--- peoria.js
+++ peoria.js
@@ -8,35 +8,13 @@
 
 /**
  * Options type for creating an element.
- * @typedef {Object} Options
- * @property {string=} type - The element type.
- * @property {string=} classList - The complete class list attribute.
- * @property {string=} innerText - The element's inner text.
- * @property {*=} onclick - The value of the element's onclick attribute.
- * @property {string=} src - The value of the element's src attribute.
- * @property {string=} style - The value of the element's style attribute.
- * @property {string=} tabindex - The value of the element's tabindex attribute.
- * @property {string=} id - The value of the element's id attribute.
+ * @typedef {{type: string, classList?: string, innerText?: string, onclick?: string, src?: string, style?: string, tabindex?: string, id?: string, child?: Element, children?: Element[], target?: string}} Options
  */
 
 /**
- * Options type for creating an element.
- * @typedef {Object} headerOptions
- * @property {{image?: string, main: string, secondary?: string, under?: string}} logo - The element type.
- * @property {string[]} links - The value of the element's id attribute.
- */
-
-/**
- * Options type for creating an element.
- * @typedef {logo: {image?: string, main: string, secondary?: string, under?: string}, links: string[]} headerOptions
- */
-
-/** @typedef {{title?: string, titles: string[]}} footerOptions */
-
-/**
- * Function to create an object in Peoria.
+ * Peoria function to create an Element.
  *
- * @param {Options} options Any and all settings are optional.
+ * @param {Options} options - Configuration for element.
  *
  * @returns {Element} The created element.
  *
@@ -55,29 +33,21 @@ function object(options) {
 	if (options.tabindex !== undefined)
 		htmlObject.setAttribute("tabindex", options.tabindex);
 	if (options.id !== undefined) htmlObject.setAttribute("id", options.id);
+	if (options.child !== undefined) htmlObject.appendChild(options.child);
+	if (options.target !== undefined)
+		htmlObject.setAttribute("target", options.target);
+	if (options.children !== undefined)
+		options.children.forEach(child => {
+			htmlObject.appendChild(child);
+		});
 	return htmlObject;
-}
-
-/**
- * Function to create a link that opens in a new tab in Peoria.
- *
- * @param {options} options Any and all settings are optional.
- *
- * @returns {Element} The created element.
- *
- * @example link({type: "a", href: "https://www.google.com", innerText: "Use Cammel Case!"});
- */
-function link(options) {
-	linkObject = object(options);
-	linkObject.setAttribute("target", "_blank");
-	return linkObject;
 }
 
 /**
  * Function to append as a child an element to another one given it's id.
  *
  * @param {string} parent The id of the parent object.
- * @param {object} child The child object.
+ * @param {Element} child The child object.
  *
  * @example append("header", headerObject);
  */
@@ -92,17 +62,16 @@ function append(parent, child) {
  * @returns {Element} <li><a></a></li> element
  */
 function createHeaderOption(text) {
-	li = object({ type: "li" });
-	li.appendChild(
-		object({
+	return object({
+		type: "li",
+		child: object({
 			type: "a",
 			classList: "header-link",
 			innerText: text.toUpperCase(),
-			onclick: text.toLowerCase() + "(), navLinks.classList.toggle('open')",
 			tabindex: "0"
-		})
-	);
-	return li;
+		}),
+		onclick: text.toLowerCase() + "(), navLinks.classList.toggle('open')"
+	});
 }
 
 /**
@@ -112,16 +81,15 @@ function createHeaderOption(text) {
  * @returns {Element} <li><a></a></li> element
  */
 function createFooterOption(text) {
-	li = object({ type: "li" });
-	li.appendChild(
-		object({
+	return object({
+		type: "li",
+		child: object({
 			type: "a",
-			innerText: "- " + text,
+			innerText: text.toUpperCase(),
 			onclick: text + "()",
 			tabindex: "0"
 		})
-	);
-	return li;
+	});
 }
 
 /** Removes all children from the object with the id "content". */
@@ -138,13 +106,13 @@ function clearContent() {
  * @param {Element} element Object to append to content.
  */
 function addToContent(element) {
-	document.getElementById("content").appendChild(element);
+	append("content", element);
 }
 
 /**
  * Creates an element and adds it to the content.
  *
- * @param {options} options Object to append to content.
+ * @param {Options} options Object to append to content.
  */
 function objectToContent(options) {
 	addToContent(object(options));
@@ -158,31 +126,43 @@ function objectToContent(options) {
  *  - <footer id "footer"></footer>.
  */
 function start() {
-	document.body.appendChild(object({ type: "header", id: "header" }));
 	document.body.appendChild(object({ type: "main", id: "content" }));
-	document.body.appendChild(object({ type: "footer", id: "footer" }));
 }
+
+/**
+ * Options type for creating an element.
+ * @typedef {{logo: {image?: string, main?: string, secondary?: string, under?: string}, links: string[]}} headerOptions
+ */
 
 /**
  * Creates a header on the <header> element.
  * @param {headerOptions} options
  */
 function header(options) {
+	document.body.appendChild(object({ type: "header", id: "header" }));
+
 	// Hamburger
-	hamburger = object({
-		type: "div",
-		classList: "hamburger",
-		onclick: "navLinks.classList.toggle('open')"
-	});
-	hamburger.appendChild(object({ type: "div", classList: "line" }));
-	hamburger.appendChild(object({ type: "div", classList: "line" }));
-	hamburger.appendChild(object({ type: "div", classList: "line" }));
+
+	append(
+		"header",
+		object({
+			type: "div",
+			classList: "hamburger",
+			onclick: "navLinks.classList.toggle('open')",
+			children: [
+				object({ type: "div", classList: "line" }),
+				object({ type: "div", classList: "line" }),
+				object({ type: "div", classList: "line" })
+			]
+		})
+	);
 
 	// Logo
+
 	topDiv = object({ type: "div", classList: "topDiv" });
 
 	if (options.logo.image !== undefined)
-		topDiv.appendChild(object({ type: "img", src: "blacklogo.png" }));
+		topDiv.appendChild(object({ type: "img", src: options.logo.image }));
 	span = object({ type: "span", classList: "collumn" });
 
 	if (options.logo.main !== undefined) {
@@ -201,47 +181,50 @@ function header(options) {
 			);
 		span.appendChild(logo);
 	}
-
 	topDiv.append(span);
+
 	if (options.logo.under !== undefined)
 		span.appendChild(object({ type: "h3", innerText: options.logo.under }));
 
 	//Navigation Links
+
 	navLinks = object({ type: "ul", classList: "nav-links" });
 	options.links.forEach(text => {
 		navLinks.appendChild(createHeaderOption(text));
 	});
 
 	//Append the items to content
-	append("header", hamburger);
 	append("header", topDiv);
 	append("header", navLinks);
 }
+
+/** @typedef {{title?: string, links: string[]} footerOptions */
 
 /**
  * Creates a footer on the <footer> element.
  * @param {footerOptions} options
  */
 function footer(options) {
-	if(options.title !== undefined) append("footer", object({ type: "h2", innerText: options.title }));
-    
-    mainDiv = object({ type: "div", classList: "row" });
-    
-	for (title in options.titles) {
-		col = object({ type: "div", classList: "column" });
-		col.appendChild(object({ type: "h3", innerText: title }));
+	document.body.appendChild(object({ type: "footer", id: "footer" }));
+
+	if (options.title !== undefined)
+		append("footer", object({ type: "h2", innerText: options.title }));
+
+	mainDiv = object({ type: "div", classList: "row" });
+
+	for (section in options.links) {
+		col = object({
+			type: "div",
+			classList: "column",
+			child: object({ type: "h3", innerText: section })
+		});
 		span = object({ type: "span" });
-		if (options.titles.hasOwnProperty(title)) {
-			options.titles[title].forEach(listLink => {
-				span.appendChild(createFooterOption(listLink));
-			});
-		}
+		options.links[section].forEach(listLink => {
+			span.appendChild(createFooterOption(listLink));
+		});
 		col.appendChild(span);
 		mainDiv.appendChild(col);
-    }
-    
+	}
+
 	append("footer", mainDiv);
 }
-
-document.cookie =
-	"AC-C=ac-c;expires=Fri, 31 Dec 9999 23:59:59 GMT;path=/;SameSite=Lax";
