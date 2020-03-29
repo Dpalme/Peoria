6885f3f2b75ab7efbf7a88e2fb575e672bb6b2fa
Dpalme <diegoPalmerin24@gmail.com>
2020-03-28 21:44:35 -0600

    sides for gutters and margins

--- margins.py
+++ margins.py
@@ -8,14 +8,17 @@ final_string = ""
 
 for margin in margin_types:
     final_string += """
-.%s-margin 								{ margin: var(--%s); }
-.%s-margin-left 						{ margin-left: var(--%s); }
-.%s-margin-right 					{ margin-right: var(--%s); }
+.%s-margin                  { margin: var(--%s); }
+.%s-margin-left             { margin-left: var(--%s); }
+.%s-margin-right            { margin-right: var(--%s); }
 .%s-margin-bottom 					{ margin-bottom: var(--%s); }
-.%s-margin-top 						{ margin-top: var(--%s); }
-.%s-margin-vertical 				{ margin-top: var(--%s);
-															margin-bottom: var(--%s); }""" % (margin, margin,
+.%s-margin-top              { margin-top: var(--%s); }
+.%s-margin-vertical         { margin-top: var(--%s);
+															margin-bottom: var(--%s); }
+.%s-margin-vertical         { margin-left: var(--%s);
+															margin-right: var(--%s); }
+                              """ % (margin, margin,
                               margin, margin, margin, margin, margin, margin,
-                              margin, margin, margin, margin, margin)
+                              margin, margin, margin, margin, margin, margin, margin, margin)
 
 pyperclip.copy(final_string)
