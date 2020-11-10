dda7ebe7259b05970f6cc4d501d41a40d7e8a724
Dpalme <diegoPalmerin24@gmail.com>
2020-11-10 13:49:46 -0600

    bootstrap naming for margin and padding

--- margins.py
+++ margins.py
@@ -4,13 +4,13 @@ final_string = ""
 
 for index in range(1, 21):
     final_string += """
-.margin-%d                      { margin: calc(%d*var(--grid));           }
-.margin-left-%d                 { margin-left: calc(%d*var(--grid));      }
-.margin-right-%d                { margin-right: calc(%d*var(--grid));     }
-.margin-bottom-%d               { margin-bottom: calc(%d*var(--grid));    }
-.margin-top-%d                  { margin-top: calc(%d*var(--grid));       }
-.margin-vertical-%d             { margin: calc(%d*var(--grid)) initial;   }
-.margin-sides-%d                { margin: initial calc(%d*var(--grid));   }
-                              """ % (index, index*5, index, index*5, index, index*5, index, index*5, index, index*5, index, index*5, index, index*5)
+.p-%d                           { padding: calc(%d*var(--grid));           }
+.pl-%d                          { padding-left: calc(%d*var(--grid));      }
+.pr-%d                          { padding-right: calc(%d*var(--grid));     }
+.pb-%d                          { padding-bottom: calc(%d*var(--grid));    }
+.pt-%d                          { padding-top: calc(%d*var(--grid));       }
+.py-%d                          { padding: calc(%d*var(--grid)) initial;   }
+.px-%d                          { padding: initial calc(%d*var(--grid));   }
+""" % (index, index*5, index, index*5, index, index*5, index, index*5, index, index*5, index, index*5, index, index*5)
 
 pyperclip.copy(final_string)
