e6ecad6263271253fdd368fa5ec53eb58935a066
Dpalme <diegoPalmerin24@gmail.com>
2020-11-12 14:53:12 -0600

    Changed back to percentage the grid system, renamed properties and altered nightmode colors

---
+++ colors.py
@@ -0,0 +1,14 @@
+import pyperclip
+
+final_string = ""
+
+colors = ["white", "mid-white", "mid-grey", "greyed-out", "black", "dark-blue", "blue", "orange", "red", "yellow", "green", "pink", "purple", "brown"]
+
+for color in colors:
+    final_string += """<div class="col-2">
+        <pre class="text-c %s-bg mb-sm">%s</pre>
+    </div>
+
+""" % (color, color)
+
+pyperclip.copy(final_string)
