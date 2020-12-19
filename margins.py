ca4859ccba492fe95efc820c240c90a251f5699f
Dpalme <diegoPalmerin24@gmail.com>
2020-12-18 21:37:27 -0600

    peoria 0.5.0

--- margins.py
+++ margins.py
@@ -2,15 +2,15 @@ import pyperclip
 
 final_string = ""
 
-for index in range(1, 21):
+for index in range(1, 4):
     final_string += """
-.p-%d                           { padding: calc(%d*var(--grid));           }
-.pl-%d                          { padding-left: calc(%d*var(--grid));      }
-.pr-%d                          { padding-right: calc(%d*var(--grid));     }
-.pb-%d                          { padding-bottom: calc(%d*var(--grid));    }
-.pt-%d                          { padding-top: calc(%d*var(--grid));       }
-.py-%d                          { padding: calc(%d*var(--grid)) initial;   }
-.px-%d                          { padding: initial calc(%d*var(--grid));   }
-""" % (index, index*5, index, index*5, index, index*5, index, index*5, index, index*5, index, index*5, index, index*5)
+.p-%d                           { padding: var(--col);                     }
+.pl-%d                          { padding-left: var(--col);                }
+.pr-%d                          { padding-right: var(--col);               }
+.pb-%d                          { padding-bottom: var(--col);              }
+.pt-%d                          { padding-top: var(--col);                 }
+.py-%d                          { padding: var(--col) initial;             }
+.px-%d                          { padding: initial var(--col);             }
+""" % (index, index, index, index, index, index, index)
 
 pyperclip.copy(final_string)
